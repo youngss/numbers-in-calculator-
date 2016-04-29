9ef79545b7c8b73f3e05425ded83d0911fe62ff4
numbers in calculator <ttasneem>
2016-04-29 16:59:27 +0200

    signs in calcularor

--- src/pro2/Design.java
+++ src/pro2/Design.java
@@ -1,5 +1,4 @@
 package pro2;
-
 import javax.script.*;
 
 
@@ -13,10 +12,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Design extends Application{
+public class Design  extends Application{
 
 	public static void main(String[] args) {
-	
+		// TODO Auto-generated method stub
 		Application.launch(args);
 
 	}
@@ -28,14 +27,17 @@ public class Design extends Application{
 	public void start(Stage st) throws Exception {
 	
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		
+		//Ïå æÍÏæ ÚÔÇä ÍÌãå ÎÏ 6 ÇÚãÏå æÕÝíä 
 	
 	text.setPrefHeight(70);
 	text.setPrefWidth(300);
 	GridPane g = new  GridPane();
 	g.add(text,0,0,6,2);	
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	 
+ 
+	Button equal = new Button("=");
+	g.add(equal, 4, 5,2,1);
+	equal.setPrefSize(103,40);
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	 
 	
@@ -51,8 +53,37 @@ public class Design extends Application{
 		    	 for(int j=2;j>=0;j--)
 		    		g.add(arr[k--], j, i);
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		    
+		   
+	     Button []arr2= new Button[10];
+	      for(int i =0; i<10 ;i++){	
+	    	  arr2[i]= new Button();
+	    	  arr2[i].setPrefSize(50,40);
+	      } 
+	      int a=0;
+		     for(int i =2 ;i<=4 ;i++){
+		    	 for(int j=3;j<=5;j++)
+		    	g.add(arr2[a++], j, i); 
+		    	 
+		    	 
+		     }
+		     arr2[0].setText("/");
+		     arr2[1].setGraphic(new ImageView("2.png"));        //åäÇ Ýí ÕÒÑå 
+		     arr2[2].setGraphic(new ImageView("3.png"));       
+		     arr2[3].setText("*");
+		     arr2[4].setText("(");
+		     arr2[5].setText(")");
+		     arr2[6].setText("-");
+		     arr2[7].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("1.png"))));       //åäÇ Ýí ÕæÑå
+		     arr2[8].setText("Sqrt");           //åäÇ Ýí ÕæÑå
+///////////////////////////////////////////////////////////////////////////////////////////////////		     
 	 
+		     String [] str = {"0",".", "%" ,"+"};
+		     Button []last_row= new Button[4];
+		    	 for(int i=0;i<4;i++){
+		    		 last_row[i]= new Button(str[i]);
+		    		  last_row[i].setPrefSize(50,40); 
+		    				 g.add(last_row[i], i, 5);	
+		    	 }
 		    	
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 			       g.setHgap(3);
@@ -64,10 +95,11 @@ public class Design extends Application{
 
 
 
+
+
    }
-}
    
-   ////////////////////////////////////////////
-	
+   
     	 	 
-  	 
+    	 
+}
